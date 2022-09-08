2e7382f16f20b48f8ead26f701aeb295aaacd8b6
Lucas Machado Vieira <lucasmvil@hotmail.com>
2022-09-08 10:01:20 -0300

    Métodos privados e estáticos

--- conta.py
+++ conta.py
@@ -7,24 +7,47 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
+
     def extrato(self):
         print("Saldo: {}".format(self.__saldo))
+
     def deposita(self, valor):
         self.__saldo += valor
+
+    def __pode_saca(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if (self.__pode_saca(valor)):
+            self.__saldo -= valor
+            print(f"Seu saldo é: {self.__saldo}")
+        else:
+            print("Você não tem saldo o suficiente!")
     def trafere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
     @property
     def saldo(self):
         return self.__saldo
+
     @property
     def titular(self):
         return self.__titular
+
     @property
     def limite(self):
         return self.__limite
+
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
